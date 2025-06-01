ad99be979b92839a4ee06bfec742f47f2301ed59
Mubelotix <mubelotix@gmail.com>
2025-06-01 17:45:42 +0200

    Add loading spinner

--- source/content.js
+++ source/content.js
@@ -27,8 +27,8 @@ export function formatNumber(num) {
     return num.toString();
 }
 
-export function loadingSpinner(customClass = "my-3") {
-	return `<svg style="box-sizing: content-box; color: var(--color-icon-primary);" width="32" viewBox="0 0 16 16" fill="none" aria-hidden="true" class="${customClass} flex-1 anim-rotate" height="32"><circle cx="8" cy="8" r="7" stroke="currentColor" stroke-opacity="0.25" stroke-width="2" vector-effect="non-scaling-stroke" fill="none"></circle><path d="M15 8a7.002 7.002 0 00-7-7" stroke="currentColor" stroke-width="2" stroke-linecap="round" vector-effect="non-scaling-stroke"></path></svg>`;
+export function loadingSpinner(customClass = "", customStyle = "") {
+	return `<svg style="box-sizing: content-box; color: var(--color-icon-primary); ${customStyle}" width="32" viewBox="0 0 16 16" fill="none" aria-hidden="true" class="${customClass} flex-1 anim-rotate" height="32"><circle cx="8" cy="8" r="7" stroke="currentColor" stroke-opacity="0.25" stroke-width="2" vector-effect="non-scaling-stroke" fill="none"></circle><path d="M15 8a7.002 7.002 0 00-7-7" stroke="currentColor" stroke-width="2" stroke-linecap="round" vector-effect="non-scaling-stroke"></path></svg>`;
 }
 
 async function init() {
