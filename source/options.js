0ced8868fcea3643cc5026ffecef1da2bd6d50cb
Mubelotix <mubelotix@gmail.com>
2025-08-02 12:38:47 +0200

    Add ace editor in settings

--- source/options.js
+++ source/options.js
@@ -2,26 +2,15 @@ import 'webext-base-css';
 import './options.css';
 import { optionsStorage } from './options-storage.js';
 
-const rangeInputs = [...document.querySelectorAll('input[type="range"][name^="color"]')];
-const numberInputs = [...document.querySelectorAll('input[type="number"][name^="color"]')];
-const output = document.querySelector('.color-output');
+import {EditorView, basicSetup} from "codemirror"
+import {yaml} from "@codemirror/lang-yaml"
 
-function updateOutputColor() {
-    output.style.backgroundColor = `rgb(${rangeInputs[0].value}, ${rangeInputs[1].value}, ${rangeInputs[2].value})`;
-}
+let editor = document.getElementById('editor');
 
-function updateInputField(event) {
-    numberInputs[rangeInputs.indexOf(event.currentTarget)].value = event.currentTarget.value;
-}
+const view = new EditorView({
+  parent: editor,
+  doc: `name: Ferdinand\nage: 93`,
+  extensions: [basicSetup, yaml()]
+})
 
-for (const input of rangeInputs) {
-    input.addEventListener('input', updateOutputColor);
-    input.addEventListener('input', updateInputField);
-}
-
-async function init() {
-    await optionsStorage.syncForm('#options-form');
-    updateOutputColor();
-}
-
-init();
+console.log("successfully loaded options.js");
