e3f4ede97fb08fd317efd61efcd83f18a8d91d97
Mubelotix <mubelotix@gmail.com>
2025-08-02 12:58:36 +0200

    Add schema

--- source/options.js
+++ source/options.js
@@ -4,13 +4,25 @@ import { optionsStorage } from './options-storage.js';
 
 import {EditorView, basicSetup} from "codemirror"
 import {yaml} from "@codemirror/lang-yaml"
+import { yamlSchema } from 'codemirror-json-schema/yaml';
+
+const schema = {
+  type: "object",
+  description: "Root configuration object",
+  properties: {
+    example: {
+      type: "boolean",
+      description: "Enable or disable the example feature",
+    },
+  },
+};
 
 let editor = document.getElementById('editor');
 
 const view = new EditorView({
   parent: editor,
   doc: `name: Ferdinand\nage: 93`,
-  extensions: [basicSetup, yaml()]
+  extensions: [basicSetup, yaml(), yamlSchema(schema)]
 })
 
 console.log("successfully loaded options.js");
